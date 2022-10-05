ad9dd966a09e1f8fb674ef3158cd37b6e317545a
jesus <a20196324@pucp.edu.pe>
2022-10-04 20:37:53 -0500

    ff

--- src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
+++ src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.pucp.lab5gtics.controller;
 
+import com.pucp.lab5gtics.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,14 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class EmployeeController {
 
+    @Autowired
+    EmployeeRepository employeeRepository;
 
     @GetMapping({"empleado/lista", "empleado"})
     public String listEmployee(Model model, @RequestParam(name = "search",required = false) String search, @RequestParam(name = "order", required = false) Integer order, RedirectAttributes attributes){
+        model.addAttribute("listaEmpleados", employeeRepository.findAll());
 
-
-        return "XXXXXX";
+        return "employee/list";
     }
 
 
