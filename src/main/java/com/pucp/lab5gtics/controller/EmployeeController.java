f1869fd57739752589ce92a952e3088cde8f7d2d
jesus <a20196324@pucp.edu.pe>
2022-10-04 21:35:33 -0500

    PUSH3

--- src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
+++ src/main/java/com/pucp/lab5gtics/controller/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
+@RequestMapping("/empleado")
 public class EmployeeController {
 
     @Autowired
@@ -23,7 +25,7 @@ public class EmployeeController {
     DepartmentRepository departmentRepository;
     JobRepository jobRepository;
 
-    @GetMapping({"empleado/lista", "empleado"})
+    @GetMapping({"/lista", ""})
     public String listEmployee(Model model, @RequestParam(name = "search",required = false) String search, @RequestParam(name = "order", required = false) Integer order, RedirectAttributes attributes){
         model.addAttribute("listaEmpleados", employeeRepository.findAll());
 
@@ -55,11 +57,11 @@ public class EmployeeController {
 
         if (optional.isPresent()) {
             model.addAttribute("employee", optional.get());
-            model.addAttribute("listaJefes", departmentRepository.findAll());
-            model.addAttribute("listaJefes", departmentRepository.findAll());
+            model.addAttribute("listaDepartamentos", departmentRepository.findAll());
+            model.addAttribute("listaTrabajos", jobRepository.findAll());
             return "employee/editFrm";
         } else {
-            return "redirect:/employee";
+            return "redirect:/empleado";
         }
 
     }
